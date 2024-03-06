d44e6c076e974ed2bf5aeb88a959b317e2bec62a
Denise Chang <ddleletan@hotmail.com>
2024-03-05 20:02:34 -0500

    complete model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -7,31 +7,28 @@
 # Pre-requisites: None
 
 #### Workspace setup ####
-library(boot)
-library(broom.mixed)
-library(collapse)
-library(dataverse)
-library(gutenbergr)
-library(janitor)
-library(knitr)
-library(marginaleffects)
-library(modelsummary)
+library(arrow)
 library(rstanarm)
-library(tidybayes)
 library(tidyverse)
 
 #### Read data ####
 analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
 
 ### Model data ####
-first_model <-
+set.seed(853)
+
+ces2020_reduced <- 
+  ces2020 |> 
+  slice_sample(n = 1000)
+
+political_preferences <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
+    voted_for ~ gender + education,
+    data = ces2020_reduced,
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    prior_intercept = 
+      normal(location = 0, scale = 2.5, autoscale = TRUE),
     seed = 853
   )
 
