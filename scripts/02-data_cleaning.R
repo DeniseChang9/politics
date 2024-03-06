1e581f481b46699b723443beb85c0b1fd679c1de
Denise Chang <ddleletan@hotmail.com>
2024-03-05 20:00:11 -0500

    clean data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,11 +9,12 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(arrow)
 
 #### Clean data ####
 ces2020 <-
   read_csv(
-    "data/raw_data/ces2020.csv",
+    here::here("data/raw_data/ces2020.csv"),
     col_types =
       cols(
         "votereg" = col_integer(),
@@ -23,7 +24,35 @@ ces2020 <-
       )
   )
 
-ces2020
+ces2020 <-
+  ces2020 |>
+  filter(votereg == 1,
+         CC20_410 %in% c(1, 2)) |>
+  mutate(
+    voted_for = if_else(CC20_410 == 1, "Biden", "Trump"),
+    voted_for = as_factor(voted_for),
+    gender = if_else(gender == 1, "Male", "Female"),
+    education = case_when(
+      educ == 1 ~ "No HS",
+      educ == 2 ~ "High school graduate",
+      educ == 3 ~ "Some college",
+      educ == 4 ~ "2-year",
+      educ == 5 ~ "4-year",
+      educ == 6 ~ "Post-grad"
+    ),
+    education = factor(
+      education,
+      levels = c(
+        "No HS",
+        "High school graduate",
+        "Some college",
+        "2-year",
+        "4-year",
+        "Post-grad"
+      )
+    )
+  ) |>
+  select(voted_for, gender, education)
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(ces2020, "data/analysis_data/analysis_data.parquet")
